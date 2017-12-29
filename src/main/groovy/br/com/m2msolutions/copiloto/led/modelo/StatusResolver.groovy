17f5829512e6ecde8462e5af491c5f4e68a11373
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2017-12-29 10:29:59 -0200

    mudando Double.MIN_VALUE e MAX_VALUE no StatusResolver para NEGATIVE_INFINITY e POSITIVE_INFINITY

--- src/main/groovy/br/com/m2msolutions/copiloto/led/modelo/StatusResolver.groovy
+++ src/main/groovy/br/com/m2msolutions/copiloto/led/modelo/StatusResolver.groovy
@@ -17,13 +17,13 @@ class StatusResolver {
     private criarStatus(){
 
         todosOsStatus = [
-            new Status(tempoMinimo: Double.MIN_EXPONENT,tempoMaximo: -3.1, codigo: 'fff80000'),
+            new Status(tempoMinimo: Double.NEGATIVE_INFINITY,tempoMaximo: -3.1, codigo: 'fff80000'),
             new Status(tempoMinimo: -2.0,tempoMaximo: -1.1, codigo: '00f80000'),
             new Status(tempoMinimo: -3.0,tempoMaximo: -2.1, codigo: '0ff80000'),
             new Status(tempoMinimo: -1.0,tempoMaximo: 1.0, codigo: '00060000'),
             new Status(tempoMinimo: 1.1,tempoMaximo: 2.0, codigo: '0001f000'),
             new Status(tempoMinimo: 2.1,tempoMaximo: 3.0, codigo: '0001ff00'),
-            new Status(tempoMinimo: 3.1,tempoMaximo: Double.MAX_VALUE ,codigo: '0001fff8')
+            new Status(tempoMinimo: 3.1,tempoMaximo: Double.POSITIVE_INFINITY ,codigo: '0001fff8')
         ]
     }
 }
