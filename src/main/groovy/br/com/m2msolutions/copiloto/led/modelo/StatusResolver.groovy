1492c28a157b215d406517ce9310fe45446a08ed
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2017-12-30 12:48:30 -0200

    arrumando intervalos no statusResolver

--- src/main/groovy/br/com/m2msolutions/copiloto/led/modelo/StatusResolver.groovy
+++ src/main/groovy/br/com/m2msolutions/copiloto/led/modelo/StatusResolver.groovy
@@ -17,13 +17,13 @@ class StatusResolver {
     private criarStatus(){
 
         todosOsStatus = [
-            new Status(tempoMinimo: Double.NEGATIVE_INFINITY,tempoMaximo: -3.1, codigo: 'fff80000'),
-            new Status(tempoMinimo: -2.0,tempoMaximo: -1.1, codigo: '00f80000'),
-            new Status(tempoMinimo: -3.0,tempoMaximo: -2.1, codigo: '0ff80000'),
+            new Status(tempoMinimo: Double.NEGATIVE_INFINITY,tempoMaximo: -3.01, codigo: 'fff80000'),
+            new Status(tempoMinimo: -2.0,tempoMaximo: -1.01, codigo: '00f80000'),
+            new Status(tempoMinimo: -3.0,tempoMaximo: -2.01, codigo: '0ff80000'),
             new Status(tempoMinimo: -1.0,tempoMaximo: 1.0, codigo: '00060000'),
-            new Status(tempoMinimo: 1.1,tempoMaximo: 2.0, codigo: '0001f000'),
-            new Status(tempoMinimo: 2.1,tempoMaximo: 3.0, codigo: '0001ff00'),
-            new Status(tempoMinimo: 3.1,tempoMaximo: Double.POSITIVE_INFINITY ,codigo: '0001fff8')
+            new Status(tempoMinimo: 1.01,tempoMaximo: 2.0, codigo: '0001f000'),
+            new Status(tempoMinimo: 2.01,tempoMaximo: 3.0, codigo: '0001ff00'),
+            new Status(tempoMinimo: 3.01,tempoMaximo: Double.POSITIVE_INFINITY ,codigo: '0001fff8')
         ]
     }
 }
