54d6aad2871203f7c10d8f7f867db494fa6b612b
Rodrigo Ribeiro <rodrigo.santos@m2msolutions.com.br>
2018-01-25 19:01:59 -0200

    criar status

--- src/main/groovy/br/com/m2msolutions/copiloto/led/CopilotoLed.groovy
+++ src/main/groovy/br/com/m2msolutions/copiloto/led/CopilotoLed.groovy
@@ -3,6 +3,7 @@ package br.com.m2msolutions.copiloto.led
 import br.com.m2msolutions.copiloto.led.helper.DespachanteDeComando
 import br.com.m2msolutions.copiloto.led.helper.DespachanteDeComandoFactory
 import br.com.m2msolutions.copiloto.led.modelo.ComandoRequest
+import br.com.m2msolutions.copiloto.led.modelo.Status
 import br.com.m2msolutions.copiloto.led.modelo.StatusResolver
 import br.com.m2msolutions.copiloto.led.modelo.modulo.Modelo
 import br.com.m2msolutions.copiloto.led.modelo.modulo.Modulo
@@ -17,10 +18,14 @@ class CopilotoLed {
     private StatusResolver statusResolver = new StatusResolver()
     private DespachanteDeComando despachante
 
+    Status obterStatus(Integer minutos){
+        statusResolver.resolveStatus minutos
+    }
+
     ComandoRequest criarRequisicao(Integer clienteId, Integer veiculoId, String modeloDoModulo, String moduloId, Integer minutos,
                                 Integer duracaoDaIluminacao = 1, Boolean openOrCloseTrip = false){
 
-        def status = statusResolver.resolveStatus minutos
+        def status = obterStatus minutos
         status.duracao = duracaoDaIluminacao
 
         new ComandoRequest(
